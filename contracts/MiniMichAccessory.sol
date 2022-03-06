5038f7348b85c9ed77b0ef57823bc15abeccf086
mini-mich <spark@mini-mich.io>
2022-03-06 08:51:33 +0800

    commit

--- contracts/MiniMichAccessory.sol
+++ contracts/MiniMichAccessory.sol
@@ -3,37 +3,21 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./ERC721A.sol";
 
-contract MiniMich is ERC721A, Ownable, Pausable, ReentrancyGuard {
+contract MiniMichAccessory is ERC721A, Ownable, Pausable, ReentrancyGuard {
     string private _metadataBaseURI;
-    bytes32 private wlisteRoot;
-    bool public preLiveToggle;
     bool public saleLiveToggle;
     bool public freezeURI;
 
-    uint256 public constant MAX_NFT = 10;//10000;
-    uint256 public constant MAX_PRESALE = 9;//999;
-    uint256 public constant MAX_PRESALE_MINT = 2;
+    uint256 public constant MAX_NFT = 200;
+    uint256 public constant MAX_MINT = 1;
     uint256 public MAX_BATCH = 5;
-    uint256 public PRESALE_COUNT = 0;
-    uint256 public PRICE = 0.02 ether;//0.5 ether;
-    uint256 public PRESALE_PRICE = 0.01 ether;//0.2 ether;
+    uint256 public PRICE = 0;
 
     address private _creators = 0xF580fc0f5aE3032171c781FBBBE73f54Fe411C4b;//to be update 0x999eaa33BD1cE817B28459950E6DcD1dA14C411f
  
-    uint256 public auctionSaleStartTime;
-    uint256 public AUCTION_START_PRICE = 0.1 ether;
-    uint256 public AUCTION_END_PRICE = 0.015 ether;
-    uint256 public AUCTION_PRICE_CURVE_LENGTH = 17 minutes;
-    uint256 public AUCTION_DROP_INTERVAL = 1 minutes;
-    uint256 public AUCTION_DROP_PER_STEP =
-        (AUCTION_START_PRICE - AUCTION_END_PRICE) /
-        (AUCTION_PRICE_CURVE_LENGTH / AUCTION_DROP_INTERVAL);
-
-
     // ** MODIFIERS ** //
     // *************** //
     modifier saleLive() {
@@ -41,28 +25,18 @@ contract MiniMich is ERC721A, Ownable, Pausable, ReentrancyGuard {
         _;
     }
 
-    modifier preSaleLive() {
-        require(preLiveToggle == true, "Presale is not live yet");
-        _;
-    }
-    modifier correctPayment(uint256 mintPrice, uint256 numToMint) {
-        require(
-            msg.value >= mintPrice * numToMint,
-            "Payment failed"
-        );
-        _;
-    }
-     modifier maxSupply(uint256 mintNum) {
+    modifier maxSupply(uint256 mintNum) {
        require(
             totalSupply() + mintNum <= MAX_NFT,
             "Sold out"
         );
         _;
      }
+
     // ** CONSTRUCTOR ** //
     // *************** //
     constructor(string memory _mURI)
-        ERC721A("MiniMich", "MM", MAX_BATCH, MAX_NFT)
+        ERC721A("MiniMichAccessory", "MMA", MAX_BATCH, MAX_NFT)
     {
         _metadataBaseURI = _mURI;
     }
@@ -109,37 +83,13 @@ contract MiniMich is ERC721A, Ownable, Pausable, ReentrancyGuard {
         payable
         nonReentrant
         saleLive
-        correctPayment(PRICE, mintNum)
         maxSupply(mintNum)
     {
-        _safeMint(_msgSender(), mintNum);
-    }
-
-    function preMint(uint256 mintNum, bytes32[] calldata merkleProof)
-        external
-        payable
-        nonReentrant
-        preSaleLive
-        correctPayment(PRESALE_PRICE, mintNum)
-    {
         require(
-            MerkleProof.verify(
-                merkleProof,
-                wlisteRoot,
-                keccak256(abi.encodePacked(_msgSender()))
-            ),
-            "Not on whitelist"
-        );
-
-        require(
-            numberMinted(msg.sender) + mintNum <= MAX_PRESALE_MINT,
+            numberMinted(msg.sender) + mintNum <= MAX_MINT,
             "Reaches wallet limit."
         );
-
-        require(PRESALE_COUNT + mintNum < MAX_PRESALE, "Presale reaches limit");
-
         _safeMint(_msgSender(), mintNum);
-        PRESALE_COUNT += mintNum;
     }
 
     function devMint(uint256 mintNum) 
@@ -201,10 +151,6 @@ contract MiniMich is ERC721A, Ownable, Pausable, ReentrancyGuard {
         saleLiveToggle = !saleLiveToggle;
     }
 
-    function tglPresale() external onlyOwner {
-        preLiveToggle = !preLiveToggle;
-    }
-
     function freezeAll() external onlyOwner {
         require(freezeURI == false, "Metadata is frozen");
         freezeURI = true;
@@ -218,20 +164,8 @@ contract MiniMich is ERC721A, Ownable, Pausable, ReentrancyGuard {
         _unpause();
     }
 
-    function updatePrice(uint256 _price, uint256 _list) external onlyOwner {
-        if (_list == 0) {
-            PRICE = _price ;
-        } else if (_list == 1) {
-            AUCTION_START_PRICE = _price ;
-        } else if (_list == 2) {
-            AUCTION_END_PRICE = _price ;
-        } else {
-            PRESALE_PRICE = _price ;
-        }
-    }
-
-    function setMerkleRoot(bytes32 _root) external onlyOwner {
-        wlisteRoot = _root;
+    function updatePrice(uint256 _price) external onlyOwner {
+        PRICE = _price ;
     }
 
     // function setOwnersExplicit(uint256 quantity)
@@ -242,41 +176,4 @@ contract MiniMich is ERC721A, Ownable, Pausable, ReentrancyGuard {
     //     _setOwnersExplicit(quantity);
     // }
 
-
-
-  function getAuctionPrice()
-    public
-    view
-    returns (uint256)
-  {
-    if (block.timestamp < auctionSaleStartTime) {
-      return AUCTION_START_PRICE;
-    }
-    if (block.timestamp - auctionSaleStartTime >= AUCTION_PRICE_CURVE_LENGTH) {
-      return AUCTION_END_PRICE;
-    } else {
-      uint256 steps = (block.timestamp - auctionSaleStartTime) /
-        AUCTION_DROP_INTERVAL;
-      return AUCTION_START_PRICE - (steps * AUCTION_DROP_PER_STEP);
-    }
-  }
-
-  function setAuctionSaleStartTime(uint256 timestamp) external onlyOwner {
-    auctionSaleStartTime = timestamp;
-  }
-
-  function auctionMint(uint256 mintNum) 
-    external 
-    payable
-    nonReentrant
-    maxSupply(mintNum)
-    correctPayment(getAuctionPrice(), mintNum)
-    {
-    require(
-      auctionSaleStartTime != 0 && block.timestamp >= auctionSaleStartTime,
-      "sale has not started yet"
-    );
-
-    _safeMint(msg.sender, mintNum);
-  }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,12 @@
 async function main() {
-   // Grab the contract factory 
-   const MmNFT = await ethers.getContractFactory("MiniMich");
+  //  const MmNFT = await ethers.getContractFactory("MiniMich");
+  //  const mmNFT = await MmNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
+  //  console.log("Contract deployed to address:", mmNFT.address);
 
-   // Start deployment, returning a promise that resolves to a contract object
-   const mmNFT = await MmNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
-   console.log("Contract deployed to address:", mmNFT.address);
+   const MmaNFT = await ethers.getContractFactory("MiniMichAccessory");
+   const mmaNFT = await MmaNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
+   console.log("Contract deployed to address:", mmaNFT.address);
+
 }
 
 main()
