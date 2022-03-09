0ba2ac30bb8de1f1e94ee71567f13c3d41608b26
mini-mich <spark@mini-mich.io>
2022-03-09 17:58:33 +0800

    commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 async function main() {
-  //  const MmNFT = await ethers.getContractFactory("MiniMich");
-  //  const mmNFT = await MmNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
-  //  console.log("Contract deployed to address:", mmNFT.address);
+   const MmNFT = await ethers.getContractFactory("MiniMich");
+   const mmNFT = await MmNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
+   console.log("Contract deployed to address:", mmNFT.address);
 
-   const MmaNFT = await ethers.getContractFactory("MiniMichAccessory");
-   const mmaNFT = await MmaNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
-   console.log("Contract deployed to address:", mmaNFT.address);
+  //  const MmaNFT = await ethers.getContractFactory("MiniMichAccessory");
+  //  const mmaNFT = await MmaNFT.deploy("https://gateway.pinata.cloud/ipfs/QmdWdwwr3SLqjRtXFGhnBP9NfVTkmpNLG3Zjx1Wtm1zDUo/")// Instance of the contract 
+  //  console.log("Contract deployed to address:", mmaNFT.address);
 
 }
 
