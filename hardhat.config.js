665500933ea552d6e92c289efeb9de369038fe9a
mini-mich <spark@mini-mich.io>
2022-03-09 18:28:09 +0800

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,27 +5,40 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require('hardhat-contract-sizer');
 require("@nomiclabs/hardhat-etherscan");
-const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY,API_URL_RINKEBY} = process.env;
+const { API_URL_MAINNET, API_URL_ROPSTEN, PRIVATE_KEY, ETHERSCAN_API_KEY, API_URL_RINKEBY } = process.env;
 module.exports = {
-   solidity: "0.8.2",
-   optimizer: {
-     enabled: true,
-     runs: 200
+   solidity: {
+      version: "0.8.2",
+      settings: {
+         optimizer: {
+            enabled: true,
+            runs: 500
+         }
+      }
    },
+   // solidity: "0.8.2",
+   // optimizer: {
+   //   enabled: true,
+   //   runs: 500,
+   // },
    defaultNetwork: "ropsten",
    networks: {
       hardhat: {},
       ropsten: {
-         url: API_URL,
+         url: API_URL_ROPSTEN,
          accounts: [`0x${PRIVATE_KEY}`]
       },
       rinkeby: {
          url: API_URL_RINKEBY,
          accounts: [`0x${PRIVATE_KEY}`]
+      },
+      mainnet: {
+         url: API_URL_MAINNET,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
    },
    etherscan: {
-     apiKey: process.env.ETHERSCAN_API_KEY
+      apiKey: process.env.ETHERSCAN_API_KEY
    },
 }
 
